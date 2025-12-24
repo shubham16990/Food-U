365ccadc4c704068817ead695a33ea5fca5be5b6
shubham kumar <shubhamk16990@gmail.com>
2025-12-24 07:42:03 +0530

    Initial commit - food website

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -27,15 +27,15 @@ const HomePage = () => {
 
     let items = useSelector((state) => state.cart.item)
 
-   let subTotal = items.reduce((total,item)=>{
-    return total+item.price * item.qty
-   },0)
-  let deliverFee = 20;
-  let taxes = subTotal*0.6/100;
-  let total = subTotal + deliverFee + taxes ;
+    let subTotal = items.reduce((total, item) => {
+        return total + item.price * item.qty
+    }, 0)
+    let deliverFee = 20;
+    let taxes = subTotal * 0.6 / 100;
+    let total = Math.floor(subTotal + deliverFee + taxes);
 
     return (
-        <div className='w-full h-screen min-h-screen max-h-screen bg-[#FFFFFF]'>
+        <div className='w-full h-screen min-h-screen max-h-screen bg-[#FFFFFF] '>
             <Navbar />
             <Collection onFilter={filterDish} />
             <div className='flex items-center justify-center overflow-hidden flex-wrap'>
@@ -47,23 +47,50 @@ const HomePage = () => {
                 })}
             </div>
             {/* Add to cart section */}
-            <div className={` w-full md:w-[75vh] h-screen shadow-xl fixed top-0 right-0 px-4 bg-[#FFFFFF] transition-all duration-500 ${showCart ? "translate-x-0" : "translate-x-full"}`} >
-                <header className='flex justify-between mt-2 p-2'>
+            <div
+                className={`w-full md:w-[75vh] h-screen shadow-xl fixed top-0 right-0 px-4 bg-[#FFFFFF]
+  transition-all duration-500 flex flex-col 
+  ${showCart ? "translate-x-0" : "translate-x-full"}`}
+            >
+
+                <header className="flex justify-between mt-2 p-2 shrink-0">
+
                     <span className='text-xl font-semibold text-green-400 cursor-pointer'>Order items</span>
                     <RxCross2 className='text-green-400 font-bold text-[18px] h-8 w-7 cursor-pointer hover:text-gray-600 '
                         onClick={() => setShowCart(false)}
                     />
                 </header>
-                <div className=" flex flex-col gap-3 mt-5
-               w-full overflow-y-auto h-screen cursor-pointer  scrollbar-hide ">
+                <div className=" flex flex-col  gap-3 mt-5 w-full overflow-y-auto h-[100%] cursor-pointer scrollbar-hide p-3   ">
                     {items.map((item) => {
-                        return <div>
+                        return <div className=''>
                             <CartCard name={item.name} image={item.image} price={item.price} id={item.id} qty={item.qty} />
                         </div>
                     })}
-                    
-                </div>  
-                
+
+                    <div className=' w-full border-y-gray-400 border-t-2 border-b-2 mt-4 flex flex-col gap-3 p-5'>
+                        <div className='flex items-center justify-between w-full '>
+                            <span className='text-xl text-gray-600 font-semibold'>Subtotal</span>
+                            <span className='text-lg text-green-400 font-semibold '>Rs {subTotal}/- </span>
+                        </div>
+                        <div className='flex items-center justify-between w-full'>
+                            <span className='text-xl text-gray-600 font-semibold '>Taxes</span>
+                            <span className='text-lg text-green-400 font-semibold '>Rs {taxes}/-</span>
+                        </div>
+                        <div className='flex items-center justify-between w-full'>
+                            <span className='text-xl text-gray-600 font-semibold '>Delivery Fee</span>
+                            <span className='text-lg text-green-400 font-semibold '>Rs {deliverFee}/-</span>
+                        </div>
+
+                    </div>
+                    <div className='flex items-center justify-between w-full p-7 '>
+                        <span className='text-xl text-gray-600 font-semibold '>Total</span>
+                        <span className='text-lg text-green-400 font-semibold '>Rs {total}/-</span>
+                    </div>
+
+                    <button className='mb-10 bg-green-500 text-xl px-5 py-2 rounded-md mt-2 hover:bg-green-400 transition-all duration-300 ease-in-out cursor-pointer hover:scale-105 hover:shadow-lg active:scale-95'>place Order</button>
+
+                </div>
+
             </div>
         </div>
     )
--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -9,12 +9,12 @@ const CartCard = ({id,name,price,qty,image}) => {
   let dispatch = useDispatch()
 
   return (
-    <div className='c] w-full h-32 rounded-xl shadow-2xl flex justify-between  '>
+    <div className='c] w-full h-32 rounded-xl shadow-xl flex justify-between p-3  '>
       <div className='flex items-center w-[60%] h-full overflow-hidden gap-5 '>
         <div className=' w-[40%] p-1  '>
           <img src={image} alt="" className='rounded-xl object-cover' />
         </div>
-        <div className='flex flex-col w-[20%] '>
+        <div className='flex flex-col w-[40%] '>
           <span className='text-md font-semibold mb-3 '>{name}</span>
           <div className='bg-gray-300 border-b-gray-300 flex rounded-xl h-7 w-full overflow-hidden px-2 font-semibold shadow-md'>
             <button className='w-[30%] bg-slate-300 h-full rounded-l-xl flex items-center justify-center t'>-</button>
@@ -25,7 +25,7 @@ const CartCard = ({id,name,price,qty,image}) => {
         </div>
       </div>
       {/* Right sight */}
-      <div className=' flex flex-col justify-start items-end gap-6 px-2'>
+      <div className=' flex flex-col justify-start items-end gap-6 px-2 mt-4'>
         <span className='text-xl text-green-400 font-semibold'>Rs {price}/-</span>
         <ImBin className='text-xl text-red-400 cursor-pointer ' onClick={()=>dispatch(RemoveItem(id))}   />
       </div>
